61f7e8dc14a1d97e6a8396e8821db58bf3f64925
Hicham Abou Jaoude <hichamoaj@me.com>
2014-07-20 00:09:13 -0400

    Fixing sltextfieldcell subview

--- Project12/SLSignUpViewController.swift
+++ Project12/SLSignUpViewController.swift
@@ -42,13 +42,18 @@ class SLSignUpViewController: UITableViewController  {
         var cell:SLTextFieldCell = self.tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as SLTextFieldCell
         
         if indexPath?.section == SLSignUpSection.username.toRaw(){
-            
+            cell.textField.placeholder = "Username"
         } else if indexPath?.section == SLSignUpSection.password.toRaw(){
-            
+            cell.textField.placeholder = "Password"
         } else if indexPath?.section == SLSignUpSection.email.toRaw(){
-            
+            cell.textField.placeholder = "Email Address"
         } else if indexPath?.section == SLSignUpSection.signUp.toRaw(){
-            
+            cell.textLabel.text = "Sign Up"
+            cell.contentView.backgroundColor =  UIColor(red: 0.84, green: 0.25, blue: 0.92, alpha: 1)
+            cell.textLabel.backgroundColor = UIColor(red: 0.84, green: 0.25, blue: 0.92, alpha: 1)
+            cell.textLabel.textColor = UIColor.whiteColor()
+            cell.textLabel.textAlignment = NSTextAlignment.Center
+            cell.textField.hidden = true
         }
         
         return cell
--- Project12/SLTextFieldCell.swift
+++ Project12/SLTextFieldCell.swift
@@ -15,11 +15,12 @@ class SLTextFieldCell: UITableViewCell {
     init(style: UITableViewCellStyle, reuseIdentifier: String!) {
         textField.font  = UIFont(name: "HelveticaNeue-Light" , size: 16.0)
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.contentView.addSubview(textField)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        textField.frame = CGRectMake(CGRectGetMinX(self.contentView.frame), CGRectGetMinY(self.contentView.frame), CGRectGetWidth(self.contentView.frame), CGRectGetHeight(self.contentView.frame))
+        textField.frame = CGRectMake(CGRectGetMinX(self.contentView.frame) + 15, CGRectGetMinY(self.contentView.frame), CGRectGetWidth(self.contentView.frame) - 15, CGRectGetHeight(self.contentView.frame))
     }
 
 }
