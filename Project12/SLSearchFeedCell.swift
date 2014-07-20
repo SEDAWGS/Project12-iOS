266b99e5481958c85c0451dd1b598da640e40ffb
Hicham Abou Jaoude <hichamoaj@me.com>
2014-07-20 05:18:56 -0400

    Layout for feed

--- Project12/SLSearchFeedCell.swift
+++ Project12/SLSearchFeedCell.swift
@@ -18,8 +18,11 @@ class SLSearchFeedCell: UICollectionViewCell {
     
     init(frame: CGRect) {
         super.init(frame: frame)
+        self.contentView.backgroundColor = UIColor.whiteColor()
         self.contentView.addSubview(header)
         self.contentView.addSubview(profilePhoto)
+        self.contentView.layer.borderColor = UIColor.blackColor().CGColor
+        self.contentView.layer.borderWidth = 0.5
         self.contentView.addSubview(personName)
         self.contentView.addSubview(subletStreet)
         self.contentView.addSubview(subletPrice)
@@ -34,8 +37,25 @@ class SLSearchFeedCell: UICollectionViewCell {
     override func layoutSubviews() {
         super.layoutSubviews()
         header.frame = CGRectMake(CGRectGetMinX(self.contentView.frame), CGRectGetMinY(self.contentView.frame), CGRectGetWidth(self.contentView.frame), CGRectGetHeight(self.contentView.frame)/7)
-        profilePhoto.frame = CGRectMake(CGRectGetMinX(self.header.frame) + 15 , CGRectGetMinY(self.header.frame) + 15, CGRectGetWidth(self.header.frame), CGRectGetHeight(self.header.frame) - 30)
-        //subletStreet.frame =
+        header.backgroundColor = UIColor.whiteColor()
+        header.layer.borderColor = UIColor.blackColor().CGColor
+        header.layer.borderWidth = 0.5
+        profilePhoto.frame = CGRectMake(CGRectGetMinX(header.frame) + 10 , CGRectGetMinY(header.frame) + 10, CGRectGetHeight(header.frame) - 20, CGRectGetHeight(header.frame) - 20)
+        profilePhoto.layer.borderColor = UIColor.blackColor().CGColor
+        profilePhoto.layer.borderWidth = 0.5
+        profilePhoto.layer.cornerRadius = profilePhoto.frame.size.height / 2
+        profilePhoto.backgroundColor = UIColor.whiteColor()
+        profilePhoto.image = UIImage(named: "profile")
+        subletStreet.frame = CGRectMake(CGRectGetMaxX(profilePhoto.frame) + 10 , CGRectGetMinY(self.profilePhoto.frame) + 20, CGRectGetWidth(header.frame) - CGRectGetMaxX(profilePhoto.frame) - 20, 23)
+        subletStreet.text = "Waterloo, Ontario"
+        subletStreet.font = UIFont(name: "HelveticaNeue-Light", size: 15)
+        subletPrice.frame = CGRectMake(CGRectGetMinX(self.contentView.frame) + 5, CGRectGetHeight(self.contentView.frame) / 2 + 20 , 50, 20)
+        subletPrice.text = "$700"
+        footer.frame = CGRectMake(CGRectGetMinX(self.contentView.frame), CGRectGetHeight(self.contentView.frame) / 1.5 + 20, CGRectGetWidth(self.contentView.frame),  CGRectGetHeight(self.contentView.frame) - (CGRectGetHeight(self.contentView.frame) / 1.5 + 20))
+        footer.layer.borderColor = UIColor.blackColor().CGColor
+        footer.layer.borderWidth = 0.5
+        footer.backgroundColor = UIColor.whiteColor()
+        quickInfo.frame = CGRectMake(CGRectGetMinX(footer.frame) + 15, CGRectGetMinY(footer.frame) + 15, CGRectGetWidth(footer.frame) - 30, CGRectGetHeight(footer.frame) - 30)
         
     }
 
--- Project12/SLSearchFeedViewController.swift
+++ Project12/SLSearchFeedViewController.swift
@@ -6,22 +6,58 @@
 //  Copyright (c) 2014 Hicham Abou Jaoude. All rights reserved.
 //
 
-class SLSearchFeedViewController: UICollectionViewController {
+class SLSearchFeedViewController: UICollectionViewController, UIScrollViewDelegate, UISearchBarDelegate {
     
+    var lastOffset: CGFloat = 0
+    var currentPage: CGFloat = 0
+    var label = UILabel()
+    
+    var textField = UITextField()
+    var searchBar = UISearchBar()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.registerClass(SLSearchFeedCell.classForCoder(), forCellWithReuseIdentifier: "Cell")
+        self.collectionView.backgroundColor = UIColor.whiteColor()
+        self.collectionView.pagingEnabled = true
+        label.font = UIFont(name: "HelveticaNeue-Light", size: 12.0)
+        label = UILabel(frame: CGRectMake(0, CGRectGetMaxY(self.view.frame) - 25, CGRectGetWidth(self.view.frame), 15))
+        label.textColor = UIColor.blackColor()
+        label.textAlignment = NSTextAlignment.Center
+        self.navigationController.view.addSubview(label)
+        self.collectionView.showsHorizontalScrollIndicator = false
+        self.collectionView.showsVerticalScrollIndicator = false
+        searchBar = UISearchBar(frame: CGRectMake(0, 0, 320, 64))
+        searchBar.delegate = self
+        var searchDisplayController = UISearchDisplayController(searchBar: searchBar, contentsController: self)
+        self.navigationItem.titleView = searchBar
+        self.navigationController.setToolbarHidden(false, animated: false)
+        self.navigationController.toolbar.barTintColor = UIColor.redColor()
     }
     
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView!) -> Int {
-        return 1
+        return 25
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar!) {
+        searchBar.showsCancelButton = true
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar!) {
+        searchBar.showsCancelButton = false
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar!)  {
+        searchBar.resignFirstResponder()
     }
     
     override func collectionView(collectionView: UICollectionView!, numberOfItemsInSection section: Int) -> Int {
-            return 25;
+        return 1;
     }
     
-//    override func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell! {
-//        
-//    }
+    override func collectionView(collectionView: UICollectionView!, cellForItemAtIndexPath indexPath: NSIndexPath!) -> UICollectionViewCell! {
+        var cell: SLSearchFeedCell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as SLSearchFeedCell;
+        return cell;
+    }
 
 }
